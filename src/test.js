2d76f01213b56c45f4fe695756015c9f84486cd0
ccscalpel <115010293@link.cuhk.edu.cn>
2022-04-23 05:00:21 -0400

    complete modifyUserInformation()

--- src/test.js
+++ src/test.js
@@ -3,6 +3,19 @@ const usersData = data.users;
 const ObjectId = require('mongodb').ObjectId;
 var PurchaseData = {};
 var userInfo = {'orderSessionHistory':[1,2,3]}
+
+
+function checkCity(city){
+  if (typeof city !== 'string') throw '';
+  var city = city.trim();
+  if (!/(^[A-Za-z\ ]+$)/.test(city)) throw '';
+  var newStr = city.split(" ");    
+  for(var i = 0; i<newStr.length; i++){
+    newStr[i] = newStr[i].slice(0,1).toUpperCase() + newStr[i].slice(1).toLowerCase();
+  }    
+  return newStr.join(" ");
+
+}
       
 async function main(){
   updatedInfo = {
@@ -14,9 +27,10 @@ async function main(){
     'zipCode': '12345'
 
   }
-  await usersData.deleteUser('626347aa689003e0050580b4');
+  console.log(checkCountry('abc'))
+  //await usersData.deleteUser('626347aa689003e0050580b4');
   //console.log(ObjectId.isValid('6249e659cc200fe94e8d11c1'))
 
 }
 
-main()
+console.log(checkCity('abc a b'))
