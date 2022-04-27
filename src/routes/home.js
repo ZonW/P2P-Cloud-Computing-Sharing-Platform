ec15e9623f7f8da8ce0d8f18b2995b78d40ed9f1
Cara-VC <carakuro9@gmail.com>
2022-04-26 20:53:26 -0400

    Add home routs and delete no use route

---
+++ src/routes/home.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+    let title = "Search an item!"
+    res.render("pages/home", { title } )
+})
+
+module.exports = router;
