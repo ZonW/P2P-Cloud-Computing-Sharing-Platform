a37ffdb917b8642c4672ce9e5b180ac4abda84b0
Cara-VC <carakuro9@gmail.com>
2022-04-26 20:23:48 -0400

    add logging

--- src/routes/logged.js
+++ src/routes/logged.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const users = require("../data/user");
+const users = require("../data/users");
 const bcryptjs = require("bcryptjs");
 
 // GET
