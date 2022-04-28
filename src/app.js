7ab57019914cb58d92eb848237d3b64806f5ee3d
ccscalpel <115010293@link.cuhk.edu.cn>
2022-04-28 17:22:24 -0400

    remove handlebars

--- src/app.js
+++ src/app.js
@@ -1,51 +1,10 @@
 const express = require('express');
 const app = express();
-const session = require('express-session');
-const static = express.static(__dirname + '/public');
-
+const static = express.static('views/pages');
 const configRoutes = require('./routes');
-const exphbs = require('express-handlebars');
 
-app.use('/public', static);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-
-app.use(
-  session({
-    name: 'AuthCookie',
-    secret: "some secret string!",
-    saveUninitialized: true,
-    resave: false,
-  })
-);
-
-app.use('/private', (req, res, next) => {
-  console.log(req.session.id);
-  if (!req.session.user) {
-    return res.redirect('/');
-  } else {
-    next();
-  }
-});
-
-app.use('/login', (req, res, next) => {
-  if (req.session.user) {
-    return res.redirect('/private');
-  } else {
-    next();
-  }
-});
-
-const logging = function (req, res, next) {
-   console.log(`[${new Date().toUTCString()}]: ${req.method} ${req.originalUrl} ${req.session.user ? '(Authenticated User)' : '(Non-Authenticated User)'}`);
-   next()
- };
- 
- app.use(logging);
-
+app.use(static);
 configRoutes(app);
 
 app.listen(3000, () => {
