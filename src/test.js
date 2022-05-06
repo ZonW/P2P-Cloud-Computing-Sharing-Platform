da3815234e96038f9ee206bf8d2e4cef13b4ab05
ZonW <wzongshuo@gmail.com>
2022-05-06 18:39:50 -0400

    add routes for TV connection

--- src/test.js
+++ src/test.js
@@ -26,28 +26,18 @@ async function main(){
     'status': 'CA',
     'country': 'US',
     'zipCode': '123435'
+
   }
 
   comment_info = {
     content: "so good",
-    rating: 1
-  }
-
-  filter_info = {
-    distance:100,
-    features: ["deepLearning"],
-    keywords: "good",
-    lat:40.7467,
-    lon:-74.0574,
-    operatingSystem:"windows",
-    price: 5,
-    sortBy: 'price'
+    rating: 7
   }
   //await usersData.createUser('zcc1234','zcc123','will', 'smith');
   //await usersData.modifyUserInformation('62688608c6990edc2c626646', updatedInfo)
   //await productsData.createProduct('62688351bc980dd1ab788583', 1,1)
   
-  const a = await productsData.filterProduct(filter_info);
+  const a = await productsData.searchProduct();
   console.log(a)
   //console.log(ObjectId.isValid('6249e659cc200fe94e8d11c1'))
 
